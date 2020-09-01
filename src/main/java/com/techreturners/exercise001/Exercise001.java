24196b71d9667be08132d6ceaaa6bdbc46788b66
Arul-Amirtha <arul.arulnathan@gmail.com>
2020-09-01 18:19:54 +0100

    Exercise 1 updation

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -4,23 +4,35 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return Character.toUpperCase(word.charAt(0))+word.substring(1);
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        return "";
+        return Character.toUpperCase(firstName.charAt(0)) + "." + Character.toUpperCase(lastName.charAt(0));
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        return 0.0;
+        double vatPrice = originalPrice + (vatRate * originalPrice / 100);
+        return (double) Math.round(vatPrice * 100) / 100;
     }
 
     public String reverse(String sentence) {
-        return "";
-    }
+        String revstring = "";
+        for (int i = sentence.length() - 1; i >= 0; i--) {
+            revstring = revstring + sentence.charAt(i);
+        }
+        return revstring;
+    };
 
     public int countLinuxUsers(List<User> users) {
-        return 0;
+        User usr;
+        int count = 0;
+        for (int i = 0; i < users.size(); i++) {
+            usr = users.get(i);
+            if (usr.getType().toUpperCase().equals("LINUX")) {
+                count++;
+            }
+        }
+        return count;
     }
 }
